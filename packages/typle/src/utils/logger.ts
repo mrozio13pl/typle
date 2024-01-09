6f59aec8bc42df9b8aae852c97609875ac097d35
mrozio13pl <41209892+mrozio13pl@users.noreply.github.com>
2024-01-09 09:24:50 +0100

    chore: logger

--- packages/typle/src/utils/logger.ts
+++ packages/typle/src/utils/logger.ts
@@ -3,17 +3,16 @@ import { getUnicode } from './util';
 
 // Simple logger.
 
-const PREFIX = 'typle';
 const logger: Record<string, (...msg: string[]) => void> = {};
 const levels = {
-    info: pc.cyan('INFO'),
-    warn: pc.yellow('WARN'),
-    error: pc.red('ERROR'),
-    success: pc.green(getUnicode('✔', '√')),
+    info: pc.cyan('info'),
+    warn: pc.yellow('warn'),
+    error: pc.red('error'),
+    success: pc.bold(pc.green(getUnicode('✔', '√'))),
 };
 
 function log(...msg: string[]): void {
-    console.log(PREFIX, ...msg);
+    console.log(...msg);
 }
 
 logger.info = (...msg: string[]) => {
